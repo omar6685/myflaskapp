0aa7b3d66165fb40b4bbd18dbd345123d175ae49
omar6685 <omr6685@hotmail.com>
2022-12-09 19:26:22 +0300

    adding mysql

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
